58521826568096606a142f23f48746d2d50f4237
Jukemal <wanvidu@gmail.com>
2020-03-22 13:56:22 +0530

    Store Interface Added.

--- app/src/main/java/com/electro/electro_cart/LoginActivity.java
+++ app/src/main/java/com/electro/electro_cart/LoginActivity.java
@@ -2,6 +2,7 @@ package com.electro.electro_cart;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -10,55 +11,89 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.electro.electro_cart.SignUpActivity.SignUpActivity;
+import com.electro.electro_cart.models.User;
+import com.electro.electro_cart.utils.EnumUserType;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     private FirebaseAuth firebaseAuth;
-    private Button btnLogin,btnSignUp;
-    private TextInputEditText txtEmail,txtPassword;
+    private TextInputEditText txtEmail, txtPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        txtEmail=findViewById(R.id.txtEmail);
-        txtPassword=findViewById(R.id.txtPassword);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtPassword = findViewById(R.id.txtPassword);
 
-        btnLogin=findViewById(R.id.btnLogin);
+        Button btnLogin = findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email=txtEmail.getText().toString();
-                String password=txtPassword.getText().toString();
+                String email = txtEmail.getText().toString();
+                String password = txtPassword.getText().toString();
 
-                if (email.isEmpty()||password.isEmpty()){
+                if (email.isEmpty() || password.isEmpty()) {
                     if (email.isEmpty()) {
-                        TextInputLayout txtEmailLayout=findViewById(R.id.txtEmailLayout);
+                        TextInputLayout txtEmailLayout = findViewById(R.id.txtEmailLayout);
                         txtEmailLayout.setError("Please enter Email!");
                     }
-                    if (password.isEmpty()){
-                        TextInputLayout txtPasswordLayout=findViewById(R.id.txtPasswordLayout);
+                    if (password.isEmpty()) {
+                        TextInputLayout txtPasswordLayout = findViewById(R.id.txtPasswordLayout);
                         txtPasswordLayout.setError("Please enter your password");
                     }
-                }else {
-                    firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this,new OnCompleteListener<AuthResult>() {
+                } else {
+                    firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()){
-                                Toast.makeText(LoginActivity.this,"Login Successful.",Toast.LENGTH_SHORT).show();
-                                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
-                                startActivity(intent);
-                                finish();
-                            }else {
-                                Toast.makeText(LoginActivity.this,"Login Error, Please Login Again",Toast.LENGTH_SHORT).show();
+                            if (task.isSuccessful()) {
+
+                                db.collection("users").document(task.getResult().getUser().getUid()).get()
+                                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                if (task.isSuccessful()) {
+                                                    DocumentSnapshot document = task.getResult();
+                                                    if (document.exists()) {
+                                                        Log.e("Login", "DocumentSnapshot data: " + document.getData());
+
+                                                        User user = document.toObject(User.class);
+
+                                                        if (user.getUserType() == EnumUserType.STORE) {
+                                                            Toast.makeText(LoginActivity.this, "Login Successful.", Toast.LENGTH_SHORT).show();
+                                                            Intent intent = new Intent(getApplicationContext(), StoreMainActivity.class);
+                                                            startActivity(intent);
+                                                            finish();
+                                                        } else {
+                                                            Toast.makeText(LoginActivity.this, "Login Successful.", Toast.LENGTH_SHORT).show();
+                                                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                                            startActivity(intent);
+                                                            finish();
+                                                        }
+                                                    } else {
+                                                        Log.e("Login", "No such document");
+                                                    }
+                                                } else {
+                                                    Log.e("Login", "get failed with ", task.getException());
+                                                    Toast.makeText(LoginActivity.this, "Login Error, Please Login Again", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Login Error, Please Login Again", Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
@@ -66,12 +101,12 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        btnSignUp=findViewById(R.id.btnSignUp);
+        Button btnSignUp = findViewById(R.id.btnSignUp);
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(), SignUpActivity.class);
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/electro/electro_cart/StoreMainActivity.java
@@ -0,0 +1,49 @@
+package com.electro.electro_cart;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class StoreMainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_store);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout_store);
+        NavigationView navigationView = findViewById(R.id.nav_view_store);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_view_store);
+
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_store_home, R.id.navigation_store_promotion, R.id.navigation_store_notification,R.id.navigation_store_profile)
+                .setDrawerLayout(drawer).build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_store);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_store);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
