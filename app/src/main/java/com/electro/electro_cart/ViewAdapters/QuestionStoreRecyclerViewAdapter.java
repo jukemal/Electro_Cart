c31a46eadcbd68ed24d6e4a220f8ab43db1ce8f2
Jukemal <wanvidu@gmail.com>
2020-03-24 21:07:19 +0530

    Store Question Added.

---
+++ app/src/main/java/com/electro/electro_cart/ViewAdapters/QuestionStoreRecyclerViewAdapter.java
@@ -0,0 +1,123 @@
+package com.electro.electro_cart.ViewAdapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.electro.electro_cart.R;
+import com.electro.electro_cart.models.Product;
+import com.electro.electro_cart.models.Question;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+
+import static com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionStoreRecyclerViewAdapter extends RecyclerView.Adapter<QuestionStoreRecyclerViewAdapter.QuestionStoreRecyclerViewHolder> {
+
+    private Context context;
+    private List<Product> productList;
+
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+    private final CollectionReference collectionReferenceProduct = db.collection("products");
+
+    public QuestionStoreRecyclerViewAdapter(Context context, List<Product> productList) {
+        this.context = context;
+        this.productList = productList;
+    }
+
+    @NonNull
+    @Override
+    public QuestionStoreRecyclerViewAdapter.QuestionStoreRecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_store_question,parent,false);
+
+        return new QuestionStoreRecyclerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull QuestionStoreRecyclerViewAdapter.QuestionStoreRecyclerViewHolder holder, int position) {
+        final Product product=productList.get(position);
+
+        Glide.with(context)
+                .load(storage.getReferenceFromUrl(product.getImage_links().get(0)))
+                .transition(withCrossFade())
+                .fitCenter()
+                .error(R.drawable.error_loading)
+                .fallback(R.drawable.error_loading)
+                .into(holder.imageView);
+
+        holder.textViewName.setText(product.getName());
+
+        holder.textViewPrice.setText(String.valueOf(product.getPrice())+" LKR");
+
+        holder.recyclerView.setHasFixedSize(true);
+        holder.recyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false));
+
+        collectionReferenceProduct.document(product.getId()).collection("questions")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        List<Question> questionList=new ArrayList<>();
+
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.e("Question", document.getId() + " => " + document.getData());
+
+                                Question question=document.toObject(Question.class);
+                                questionList.add(question);
+                            }
+
+                            QuestionRecyclerViewAdapter questionRecyclerViewAdapter=new QuestionRecyclerViewAdapter(context,questionList,product.getId());
+                            holder.recyclerView.setAdapter(questionRecyclerViewAdapter);
+                        } else {
+                            Log.e("Question", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    public class QuestionStoreRecyclerViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageView;
+        TextView textViewName;
+        TextView textViewPrice;
+        RecyclerView recyclerView;
+
+        public QuestionStoreRecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.imgThumb_txt_name_question_store);
+            textViewName = itemView.findViewById(R.id.txt_name_question_store);
+            textViewPrice = itemView.findViewById(R.id.txt_price_question_store);
+            recyclerView = itemView.findViewById(R.id.recyclerView_question_store);
+        }
+    }
+}
---
+++ app/src/main/java/com/electro/electro_cart/ui/store/QuestionStoreFragment.java
@@ -0,0 +1,82 @@
+package com.electro.electro_cart.ui.store;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.electro.electro_cart.R;
+import com.electro.electro_cart.ViewAdapters.QuestionStoreRecyclerViewAdapter;
+import com.electro.electro_cart.models.Product;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionStoreFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private QuestionStoreRecyclerViewAdapter questionStoreRecyclerViewAdapter;
+
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+    private final CollectionReference collectionReferenceProduct = db.collection("products");
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root=inflater.inflate(R.layout.fragment_store_question,container,false);
+
+        ProgressBar progressBar=root.findViewById(R.id.progressBar_fragment_question_store);
+
+        recyclerView=root.findViewById(R.id.recyclerView_fragment_question_store);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false));
+
+        collectionReferenceProduct
+                .whereEqualTo("storeId",firebaseAuth.getCurrentUser().getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            final List<Product> productList = new ArrayList<>();
+
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.e("Question Fragment", document.getId() + " => " + document.getData());
+
+                                Product product=document.toObject(Product.class);
+                                productList.add(product);
+                            }
+
+                            questionStoreRecyclerViewAdapter=new QuestionStoreRecyclerViewAdapter(getActivity(),productList);
+                            progressBar.setVisibility(View.GONE);
+                            recyclerView.setAdapter(questionStoreRecyclerViewAdapter);
+                        } else {
+                            Log.e("Question Fragment", "Error getting documents: ", task.getException());
+
+                            progressBar.setVisibility(View.GONE);
+                            recyclerView.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
+        return root;
+    }
+}
