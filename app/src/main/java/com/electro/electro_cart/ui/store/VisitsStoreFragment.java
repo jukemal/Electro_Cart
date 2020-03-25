035f7524b6f76eb33ac33bc0fbb3b3eda8e4fd69
Jukemal <wanvidu@gmail.com>
2020-03-25 09:58:10 +0530

    Store Visits Added.

--- app/src/main/java/com/electro/electro_cart/ui/store/VisitsStoreFragment.java
+++ app/src/main/java/com/electro/electro_cart/ui/store/VisitsStoreFragment.java
@@ -1,23 +1,125 @@
 package com.electro.electro_cart.ui.store;
 
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.electro.electro_cart.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IFillFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.view.LineChartView;
+import lombok.SneakyThrows;
 
 public class VisitsStoreFragment extends Fragment {
 
+    LineChartView lineChartView;
+
+
+    @SneakyThrows
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_store_visites,container,false);
+        View view = inflater.inflate(R.layout.fragment_store_visites, container, false);
 
-        return  view;
+        lineChartView = view.findViewById(R.id.chart1);
+
+        List yAxisValues = new ArrayList();
+        List axisValues = new ArrayList();
+
+
+        Line line = new Line(yAxisValues).setColor(Color.parseColor("#9C27B0"));
+
+        String today = new SimpleDateFormat("MM-dd", Locale.getDefault()).format(new Date());
+        Log.e("Today",today);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd",Locale.ENGLISH);
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(sdf.parse(today));
+
+        List<String> axisData =new ArrayList<>();
+
+        for (int i=1;i<=14;i++){
+            c.add(Calendar.DATE,-1);
+            String date=sdf.format(new Date(c.getTimeInMillis()));
+            axisData.add(date);
+            Log.e("Date",date);
+        }
+
+        for (int i = 0; i < axisData.size(); i++) {
+            axisValues.add(i, new AxisValue(i).setLabel(axisData.get(axisData.size()-i-1)));
+        }
+
+        Random random=new Random();
+
+        for (int i = 0; i < axisData.size(); i++) {
+            yAxisValues.add(new PointValue(i, random.nextInt(150)));
+        }
+
+        List lines = new ArrayList();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        Axis axis = new Axis();
+        axis.setValues(axisValues);
+        axis.setTextSize(16);
+        axis.setTextColor(Color.parseColor("#03A9F4"));
+        data.setAxisXBottom(axis);
+
+        Axis yAxis = new Axis();
+        yAxis.setName("Visit Count");
+        yAxis.setTextColor(Color.parseColor("#03A9F4"));
+        yAxis.setTextSize(16);
+        data.setAxisYLeft(yAxis);
+
+        lineChartView.setLineChartData(data);
+        Viewport viewport = new Viewport(lineChartView.getMaximumViewport());
+        viewport.top = 160;
+        lineChartView.setMaximumViewport(viewport);
+        lineChartView.setCurrentViewport(viewport);
+
+        return view;
     }
+
+
 }
