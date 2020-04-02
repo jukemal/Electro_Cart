7befd8c336b844e037040b2eb88e5da62ba021fb
Jukemal <wanvidu@gmail.com>
2020-04-02 18:23:34 +0530

    Promotion Updated.

--- app/src/main/java/com/electro/electro_cart/ViewAdapters/PromotionRecyclerViewAdapter.java
+++ app/src/main/java/com/electro/electro_cart/ViewAdapters/PromotionRecyclerViewAdapter.java
@@ -1,24 +1,56 @@
 package com.electro.electro_cart.ViewAdapters;
 
 import android.content.Context;
+import android.graphics.Paint;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.electro.electro_cart.R;
+import com.electro.electro_cart.models.Product;
+import com.electro.electro_cart.models.Promotion;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+import static com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade;
 
 public class PromotionRecyclerViewAdapter extends RecyclerView.Adapter<PromotionRecyclerViewAdapter.PromotionRecyclerViewHolder> {
 
-    Context context;
+    private Context context;
+    private List<Product> products;
+    NavController navController;
 
-    int[] sliderImages = {R.drawable.image_slide_1, R.drawable.image_slide_2, R.drawable.image_slide_3, R.drawable.image_slide_4, R.drawable.image_slide_5};
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
 
-    public PromotionRecyclerViewAdapter(Context context) {
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+    private final CollectionReference collectionReferenceProduct = db.collection("products");
+
+    public PromotionRecyclerViewAdapter(Context context, List<Product> products,NavController navController) {
         this.context = context;
+        this.products = products;
+        this.navController=navController;
     }
 
     @NonNull
@@ -31,22 +63,135 @@ public class PromotionRecyclerViewAdapter extends RecyclerView.Adapter<Promotion
 
     @Override
     public void onBindViewHolder(@NonNull PromotionRecyclerViewAdapter.PromotionRecyclerViewHolder holder, int position) {
-        holder.imageView.setImageResource(sliderImages[position]);
+        final Product product = products.get(position);
+
+        final Promotion promotion= product.getPromotion();
+
+        Glide.with(context)
+                .load(storage.getReferenceFromUrl(product.getImage_links().get(0)))
+                .transition(withCrossFade())
+                .fitCenter()
+                .error(R.drawable.error_loading)
+                .fallback(R.drawable.error_loading)
+                .into(holder.imageView);
+
+        holder.textViewName.setText(product.getName());
+
+        if (promotion==null){
+            holder.textViewPrice.setText(String.valueOf(product.getPrice())+" LKR");
+        }else {
+            int price=product.getPrice()*(100-promotion.getDiscountPercentage())/100;
+
+            holder.textViewPrice.setText(String.valueOf(price)+" LKR");
+
+            holder.textViewPriceDiscount.setText(String.valueOf(product.getPrice())+" LKR");
+
+            holder.textViewPriceDiscount.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+
+            holder.textViewPriceDiscount.setVisibility(View.VISIBLE);
+
+            holder.textViewPromotionBadge.setVisibility(View.VISIBLE);
+        }
+
+        holder.ratingBar.setRating(product.getRating());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View dialogView= LayoutInflater.from(context).inflate(R.layout.layout_promotion_list_popup,null);
+
+                MaterialAlertDialogBuilder builder=new MaterialAlertDialogBuilder(context);
+                builder.setView(dialogView);
+                final AlertDialog alertDialog=builder.show();
+
+                ImageView imageViewPicture=dialogView.findViewById(R.id.image);
+
+                Glide.with(context)
+                        .load(storage.getReferenceFromUrl(product.getImage_links().get(0)))
+                        .transition(withCrossFade())
+                        .fitCenter()
+                        .error(R.drawable.error_loading)
+                        .fallback(R.drawable.error_loading)
+                        .into(imageViewPicture);
+
+                TextView textViewName=dialogView.findViewById(R.id.txt_name_item_product);
+
+                textViewName.setText(product.getName());
+
+                TextView textViewPrice=dialogView.findViewById(R.id.txt_price_item_product);
+
+                int price=product.getPrice()*(100-promotion.getDiscountPercentage())/100;
+
+                textViewPrice.setText(String.valueOf(price)+" LKR");
+
+                TextView textViewPriceDiscount=dialogView.findViewById(R.id.textPriceDiscount);
+
+                textViewPriceDiscount.setText(String.valueOf(product.getPrice())+" LKR");
+
+                textViewPriceDiscount.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+
+                RatingBar ratingBar=dialogView.findViewById(R.id.product_rating_product);
+
+                ratingBar.setRating(product.getRating());
+
+                TextView textViewPromotionDescription=dialogView.findViewById(R.id.description);
+
+                textViewPromotionDescription.setText(promotion.getDescription());
+
+                TextView textViewDate=dialogView.findViewById(R.id.date);
+
+                SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MMM-dd", Locale.ENGLISH);
+
+                textViewDate.setText("Grab This Item Before "+simpleDateFormat.format(promotion.getEndDate())+" for "+String.valueOf(promotion.getDiscountPercentage())+"% Off");
+
+                Button buttonGo=dialogView.findViewById(R.id.btn);
+
+                buttonGo.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        alertDialog.dismiss();
+                        Bundle bundle = new Bundle();
+                        bundle.putString("id", product.getId());
+                        navController.navigate(R.id.action_to_navigation_product, bundle);
+                    }
+                });
+
+                ImageView buttonClose=dialogView.findViewById(R.id.close);
+
+                buttonClose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        alertDialog.dismiss();
+                    }
+                });
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return sliderImages.length;
+        return products.size();
     }
 
     public class PromotionRecyclerViewHolder extends RecyclerView.ViewHolder{
 
         ImageView imageView;
+        TextView textViewName;
+        TextView textViewPrice;
+        TextView textViewPriceDiscount;
+
+        RatingBar ratingBar;
+
+        TextView textViewPromotionBadge;
 
         public PromotionRecyclerViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            imageView=itemView.findViewById(R.id.imgThumb_item_promotion);
-        }
+            imageView = itemView.findViewById(R.id.imgThumb_item_product);
+            textViewName = itemView.findViewById(R.id.txt_name_item_product);
+            textViewPrice = itemView.findViewById(R.id.txt_price_item_product);
+            textViewPriceDiscount = itemView.findViewById(R.id.textPriceDiscount);
+            ratingBar = itemView.findViewById(R.id.product_rating_product);
+            textViewPromotionBadge = itemView.findViewById(R.id.promotion_badge);        }
     }
 }
--- app/src/main/java/com/electro/electro_cart/ui/promotions/PromotionsFragment.java
+++ app/src/main/java/com/electro/electro_cart/ui/promotions/PromotionsFragment.java
@@ -1,6 +1,7 @@
 package com.electro.electro_cart.ui.promotions;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,21 +14,44 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.electro.electro_cart.R;
+import com.electro.electro_cart.ViewAdapters.ProductListRecycleViewAdapter;
 import com.electro.electro_cart.ViewAdapters.PromotionRecyclerViewAdapter;
+import com.electro.electro_cart.models.Product;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PromotionsFragment extends Fragment {
 
     private RecyclerView recyclerView;
+    private PromotionRecyclerViewAdapter promotionRecyclerViewAdapter;
 
-    PromotionRecyclerViewAdapter promotionRecyclerViewAdapter;
+    NavController navController;
+
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+    private final CollectionReference collectionReferenceProduct = db.collection("products");
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_promotions, container, false);
+
+        navController= Navigation.findNavController(container);
 
         ProgressBar progressBar=root.findViewById(R.id.progressBar_promotion);
 
@@ -35,9 +59,29 @@ public class PromotionsFragment extends Fragment {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false));
 
-        promotionRecyclerViewAdapter=new PromotionRecyclerViewAdapter(getActivity());
-        progressBar.setVisibility(View.GONE);
-        recyclerView.setAdapter(promotionRecyclerViewAdapter);
+        collectionReferenceProduct
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        final List<Product> productList = new ArrayList<>();
+
+                        if (task.isSuccessful()){
+                            for (QueryDocumentSnapshot product : task.getResult()) {
+                                Product p = product.toObject(Product.class);
+                                if (p.getPromotion()!=null){
+                                    productList.add(p);
+                                }
+                            }
+
+                            promotionRecyclerViewAdapter=new PromotionRecyclerViewAdapter(getActivity(),productList,navController);
+                            progressBar.setVisibility(View.GONE);
+                            recyclerView.setAdapter(promotionRecyclerViewAdapter);
+                        }else {
+
+                        }
+                    }
+                });
 
         return root;
     }
